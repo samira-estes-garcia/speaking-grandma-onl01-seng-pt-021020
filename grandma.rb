d011a833f35b7a29b06c5d9effa3a14a01267d38
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-18 21:18:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
-    puts "NO, NOT SINCE 1938!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == 'I LOVE YOU GRANDMA!'
-    puts "I LOVE YOU TOO PUMPKIN!" "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
