bfc5f1096180f09df0d505c96edc3a8ebdc03a81
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-18 21:14:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase
+  if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   elsif phrase == 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
